396b75ae0dbd6bc9fb5b33397ef7d85f9074e397
Joe <development@inglenookdevelopment.com>
2019-02-17 21:20:19 +0000

    check in sample app.

---
+++ sample/RippleBackgroundSample/MainActivity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Android.App;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using Xama.JTPorts.RippleBackground;
+
+namespace RippleBackgroundSample
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private RippleBackground rippleBackground; 
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            rippleBackground = FindViewById<RippleBackground>(Resource.Id.content);
+            rippleBackground.RippleColour = Color.DarkRed;
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+        }
+        
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+	}
+}
+
